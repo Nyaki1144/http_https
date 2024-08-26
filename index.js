d0f9e6889a0bb6845c1c0c79585b9bb9bc3e44ca
nayki1144 <narek1144@gmail.com>
2024-08-26 21:23:47 +0400

    created http site

---
+++ index.js
@@ -0,0 +1,28 @@
+// server.js
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = 8005;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/" || req.url === "/index.html") {
+    const filePath = path.join(__dirname, "index.html");
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found /\\oOo/\\");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
